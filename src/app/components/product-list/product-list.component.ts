ecd17753e0226c982598a72736cbb0dd86db5418
Rashen <rashen598@gmail.com>
2023-11-30 07:48:28 +0530

    updated the product list comp to get the products by category id

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,27 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit{
 
   products : Product[] = [];
+  currentCategoryId : number = 1;
 
-  constructor(private productService : ProductService){
+  constructor(private productService : ProductService,
+              private route : ActivatedRoute){
 
   }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe( () => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    //Check if "id" parameter is available
+    //snapshot -> state of route at this given moment
+    //paramMap -> Map of all the route parameters
+    //has -> read the id parameter
+    const hasCategoryId : boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      //get the "id" param string, convert string to a number using the symbol "+"
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      //default to category 1
+      this.currentCategoryId = 1;
+    }
+
+    //get the product by category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,7 +14,10 @@ export class ProductService {
 
   //Returns and observable
   //Map the JSON data from Spring Data REST to product array
-  getProductList() : Observable<Product[]> {
+  getProductList(categoryId : number) : Observable<Product[]> {
+
+    //@TODO need to buld URL based on category id in the backend
+
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
