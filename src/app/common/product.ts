838ddd8324982ea81c0eeb3905f6db33af026d94
Rashen <rashen598@gmail.com>
2023-12-05 15:34:23 +0530

    updated product class

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,14 +1,14 @@
 export class Product {
-
-    constructor(public sku : String,
-            public name : String,
-            public description : String,
-            public unitPrice : number,
-            public imageUrl : String, 
-            public active : boolean,
-            public unitsInStock : number,
-            public dateCreated : Date,
-            public lastUpdated : Date){
-
-    }
+  constructor(
+    public id: String,
+    public sku: String,
+    public name: String,
+    public description: String,
+    public unitPrice: number,
+    public imageUrl: String,
+    public active: boolean,
+    public unitsInStock: number,
+    public dateCreated: Date,
+    public lastUpdated: Date
+  ) {}
 }
