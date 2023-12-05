51e5dac297e3fc13c2698000fd51f3c834303f06
Rashen <rashen598@gmail.com>
2023-12-05 16:06:46 +0530

    updated the product-details.ts file

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product!: Product; //! -> tells typescript compiler to suspend null and undefined checks for this property
 
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    });
+  }
+
+  handleProductDetails() {
+    //getting the id and converting it to a number
+    const productId = +this.route.snapshot.paramMap.get('id')!;
+
+    this.productService.getProduct(productId).subscribe((data) => {
+      this.product = data;
+    });
+  }
 }
